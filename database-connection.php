dea83fdaf827f457aa397de682c8a72ed60ff00c
Sumit Sinha <sumitsinha1989@gmail.com>
2014-09-04 17:46:58 +0530

    Added a _globals.php sample file

--- database-connection.php
+++ database-connection.php
@@ -1,5 +1,5 @@
 <?php
-require_once '_globals.php'; 
+require_once '_globals.php'; //File with all the passwords and sensitive data
 //Make Server Connection
 $con = mysql_connect($GLOBALS['MYSQL_DOMAIN'], $GLOBALS['MYSQL_USERNAME'], $GLOBALS['MYSQL_PASSWORD']) or die ('MySQL Error in connection');
 mysql_select_db($GLOBALS['MYSQL_DATABASE'], $con) or die('MySQL Error in database selection');
