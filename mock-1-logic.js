c7bc2c811646c52c8067e8beb541938a7c4d7344
Sumit Sinha <sumitsinha1989@gmail.com>
2014-09-23 15:04:40 +0530

    HPT #7, #5 show relevant tweets wrt label in tweetpage

--- mock-1-logic.js
+++ mock-1-logic.js
@@ -1,18 +1,14 @@
+var getlabel = location.search.split('label=')[1];
+
 var myApp = angular.module('labelpage', []);
 // this controller brings the JSON data from a specified paths. 
 myApp.controller('MainCtrl', ['$scope','$http', function ($scope,$http) {
         
         $http.get('host.php?flag=labelretrieval').success (function(data){
             //labels stores the JSON data which carries the Label details with ID
-                $scope.labels=data;
-                
+                $scope.labels=data;             
              //JSON format is {"1":"humour","2": "business"..};
-         //   var i=1; 
-         //   alert(data[i]);
-                //for(var i=0; i<data.Length;i++)
-          //      alert(data.Length);
-               
-                  
+            
         });
     
 /*	$scope.labels = {};
@@ -38,19 +34,20 @@ var myApp = angular.module('tweetpage', []);
 myApp.controller('MainCtrl', ['$scope','$http', function ($scope,$http) {
         
         $http.get('host.php?flag=fetch').success (function(data){
-
-                $scope.tweets=data;               
-                  
+                $scope.tweets=data;
+				$scope.gtlabel=getlabel;
         });   
 }]);
 myApp.filter('unsafe', function($sce) {
-2
+
     return function(val) {
-3
+
         return $sce.trustAsHtml(val);
-4
+
     };
-5
+
 });
+
+
 
 
