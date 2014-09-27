413fc309ff1ce1740c67cabba483a6f3e8522759
Sumit Sinha <sumitsinha1989@gmail.com>
2014-09-27 14:42:52 +0530

    Diont accept a tweet twice for same lable

--- host.php
+++ host.php
@@ -26,29 +26,47 @@ require_once 'database-connection.php';
 
 if ($flag=="put") //put flag means the mod wants to push some tweets in the database
 {
-    $url="https://api.twitter.com/1/statuses/oembed.json?id=$tweetid&omit_script=true";
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_URL,$url);
-    $result=curl_exec($ch);
-    curl_close($ch);//curl is used to get the oembed code of the perticular tweet from twitter servers with the help of tweet id
-    $embed=json_decode($result, true);
-    $embed=$embed["html"];
-    $labelquery=mysql_query("select `label-id` from label where `label-name` = '$label';");
-    $labelid = mysql_fetch_array($labelquery);
-    $labelid=$labelid['label-id'];
-    $query = "INSERT INTO tweets VALUES ($tweetid,$labelid,'$embed');";
-    if(mysql_query($query, $con))
+   $alreadythere=mysql_query("select `label-name` from tweets inner join label on `label-id`=`tweet-label-id` where `tweet-id` = $tweetid;");
+    while($labelalready = mysql_fetch_array($alreadythere, MYSQL_ASSOC))
     {
-        $response_array['status'] = 'Done!';  
+
+       if($label==$labelalready['label-name'])
+       {
+           $already=1;
+           break;
+       }
+    }
+    
+    if($already==1)
+    {
+        $response_array['status'] = 'Already there ;)';
     }
     else
     {
-        $response_array['status'] = 'MySql Error :(';  
+        $url="https://api.twitter.com/1/statuses/oembed.json?   id=$tweetid&omit_script=true";
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_URL,$url);
+        $result=curl_exec($ch);
+        curl_close($ch);//curl is used to get the oembed code of the perticular tweet from twitter servers with the help of tweet id
+        $embed=json_decode($result, true);
+        $embed=$embed["html"];
+        $labelquery=mysql_query("select `label-id` from label where `label-name` = '$label';");
+        $labelid = mysql_fetch_array($labelquery);
+        $labelid=$labelid['label-id'];
+        $query = "INSERT INTO tweets VALUES ($tweetid,$labelid,'$embed');";
+        if(mysql_query($query, $con))
+        {
+            $response_array['status'] = 'Done!';  
+        }
+        else
+        {
+            $response_array['status'] = 'MySql Error :(';  
+        }
     }
-    header('Content-type: application/json'); //setting the response header
-    echo json_encode($response_array);
+        header('Content-type: application/json'); //setting the response header
+        echo json_encode($response_array);
 }
 
 else if ($flag=="fetch") //flag fetch indicates the user wants all tweets in JSON format
